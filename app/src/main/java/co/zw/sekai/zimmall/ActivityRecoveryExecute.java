f8682f5ce7752277b92cdbccaaa2b2afd8fe9a18
UncleFrey <alder3227@usa.com>
2020-07-14 13:49:01 +0200

    Added Pin Recovery Functions

--- app/src/main/java/co/zw/sekai/zimmall/ActivityRecoveryExecute.java
+++ app/src/main/java/co/zw/sekai/zimmall/ActivityRecoveryExecute.java
@@ -3,12 +3,47 @@ package co.zw.sekai.zimmall;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class ActivityRecoveryExecute extends AppCompatActivity {
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class ActivityRecoveryExecute extends AppCompatActivity implements View.OnClickListener{
+    //Dec Views
+    private TextView txtQst;
+    private EditText editAns;
+    private Button btnProceed;
+
+    //Dec Firebase
+    private DatabaseReference recoveryRef;
+
+    String phone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recovery_execute);
+
+        phone = getIntent().getStringExtra("phone");
+
+        recoveryRef = FirebaseDatabase.getInstance().getReference().child("Recovery Options")
+                .child(phone);
+        
+        //Bind views
+        txtQst = findViewById(R.id.textViewQst);
+        editAns = findViewById(R.id.editTextAns);
+        btnProceed = findViewById(R.id.buttonProceed);
+
+        btnProceed.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == btnProceed){
+
+        }
     }
 }
